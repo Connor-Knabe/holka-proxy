ca228d50d3b81adb73c3f065aa484caaf0e14435
ObiwanBox <Connor-Knabe@users.noreply.github.com>
2020-10-31 08:17:38 -0500

    upgrading helmet

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var https = require('https');
 var app = express();
 var http = require('http');
 var helmet = require('helmet');
+var login = require('./login.js');
+
 var waiverOptions = {
     target: 'http://localhost:2612',
     changeOrigin: false,
@@ -52,8 +54,8 @@ var atbOptions = {
 };
 
 var serverOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/smiil.es/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/smiil.es/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/'+login.site'/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/'login.site+'/fullchain.pem'),
     secureProtocol: 'TLSv1_2_method',
     secureOptions: constants.SSL_OP_NO_SSLv3
 };
@@ -69,6 +71,11 @@ app.use('/proxy/fbook', proxy(fbookWebhookOptions));
 app.use('/proxy/hnsa', proxy(hnsaOptions));
 app.use('/proxy/hsb', proxy(hsbOptions));
 app.use('/proxy/atb', proxy(atbOptions));
-app.use('/', proxy({ target: 'http://localhost:8080', changeOrigin: false }));
+//app.use('/', proxy({ target: 'http://localhost:8080', changeOrigin: false }));
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+
 http.createServer(app).listen(80);
 https.createServer(serverOptions, app).listen(443);
