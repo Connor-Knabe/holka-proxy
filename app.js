5cee76f45b5b3ee86db7595157c69223fb75dc07
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2023-01-11 06:17:25 -0600

    updating packages

--- app.js
+++ app.js
@@ -54,20 +54,20 @@ var fpaOptions = {
 	xfwd:true
 };
 
-var serverOptions = {
+/*var serverOptions = {
     key: fs.readFileSync('/etc/letsencrypt/live/'+login.site+'/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/'+login.site+'/fullchain.pem'),
     secureProtocol: 'TLSv1_2_method',
     secureOptions: constants.SSL_OP_NO_SSLv3
-};
+};*/
 app.use(helmet());
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
     res.set('Content-Security-Policy', '');
     if (!req.secure) {
 //        return res.redirect(['https://', req.get('Host'), req.url].join(''));
     }
     next();
-});
+});*/
 app.use('/proxy/waiver', createProxyMiddleware(waiverOptions));
 app.use('/proxy/hnsa', createProxyMiddleware(hnsaOptions));
 app.use('/proxy/hsb', createProxyMiddleware(hsbOptions));
@@ -79,5 +79,5 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
-http.createServer(app).listen(80);
-https.createServer(serverOptions, app).listen(443);
+http.createServer(app).listen(3000);
+//https.createServer(serverOptions, app).listen(443);
